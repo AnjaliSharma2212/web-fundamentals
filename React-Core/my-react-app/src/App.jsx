c6b258cce36a6fa0d02861673d512cd6488b55ea
Anjali Sharma <anjalivce19@gmail.com>
2026-02-25 19:37:40 +0530

    react-core

--- React-Core/my-react-app/src/App.jsx
+++ React-Core/my-react-app/src/App.jsx
@@ -1,33 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
-function App() {
+//Functional Components -> its a javascript function that returns JSX(JavaScript XML)
+// JSX -> JSX is nothing but JavaScript XML 
+//JSX means writing HTML structure like code inside javascript
+function Greet({ name }) {
+  return (
+    <>
+      <p>Hello, Welcome  {name} Good Evening.</p>
+    </>
+  )
+}
+
+//PROPS
+//props are used to pass the data from parent to child component
+// props are read only and passed like HTML attributes
+
+function User(props) {
+  return (
+    <>
+      <p>This is props: {props.name}</p>
+    </>
+  )
+}
+//STATE -> state is data that belongs to components and may be change over time
+// state is used to Update the UI  when something changes
+
+function PropsExample({ title }) {
   const [count, setCount] = useState(0)
+  return (
+    <>
+    {/* props */}
+      <h2>DemoProps</h2>
+      <h3>{title}</h3>
+
+     {/* state */}
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button></>
+  )
+}
+
+function ControlledComponents(){
+  const [name, setName]= useState("")
+  const [email, setEmail]= useState("")
+
+
+  function handleSubmit(e){
+    e.preventDefault()
+    alert(`Name ${name} Email: ${email}`)
+    setName("")
+    setEmail("")
+  }
+// useEffect example: tomanage side effects
+  function Timer(){
+    const [seconds, setSeconds]= useState(0)
+    // runs after every render when second changes
+    useEffect(()=>{
+     const interval= setInterval(()=>{
+           setSeconds((prev)=> prev+1)
+        },1000) // second
+        //cleanup Function
+         return()=> clearInterval(interval)
+    },[]) // runs on mount , empty dependency array
+  }
+  return(
+    <>
+     <form onSubmit={handleSubmit}>
+      <input type="text" value={name} placeholder='Enter the Name' onClick={(e)=> setName(e.target.value)}/>
+      <input type='email' value={email} placeholder='Enter the email' onClick={(e)=> setEmail(e.target.value)}/>
+      <button type='submit'>Submit</button>
+     </form>
+    </>
+  )
+}
+function App() {
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       {/* JSX props vs state  */}
+      <Greet name="Anjali" />
+      <User name="Alice" />
+      <PropsExample title="Props vs State Demo" />
+
+      {/* controlled components */}
+      <ControlledComponents/>
+
+      {/* useeffect -> side effects*/}
+      <Timer/>
     </>
   )
 }
